8914678819abec89180b316822a940c356a90a7f
alextumanoff <tumanoff@gmail.com>
2024-05-21 21:40:22 +0000

    Added member.js

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+  return {
+    name: 'Member',
+    skills: [
+      {
+        name: 'React',
+        level: 4,
+      },
+      {
+        name: 'Angular',
+        level: 3,
+      },
+      {
+        name: 'Vue',
+        level: 2,
+      },
+    ],
+  };
+}
